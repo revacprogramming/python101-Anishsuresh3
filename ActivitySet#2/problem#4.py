12a02327181f671ae569eb2ce920b391dde849ec
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-05-26 16:37:17 +0000

    ..

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,18 +1,13 @@
 
 
 def get_cs():
-    st=input()
-    return st
-
-
+    return(input())
 def cs_to_lot(cs):
     j=[]
     ll=cs.split(';')
     for i in ll:
        j.append(tuple(i.split('=')))
     return j
-
-
 def lot_to_cs(lot):
     stt=''
     for a,b in lot:
@@ -26,8 +21,8 @@ def main():
     lot=cs_to_lot(cs)  # convert connect string to list of tuples
     print(lot)
 
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
+    #cs=lot_to_cs(lot)  # convert list of strings to connect string
+    #print(cs)
 
 
 if __name__ == '__main__':
--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,21 @@
 
 
 class Menu:
-    """fill in class definition here"""
+  price=[]
+  name=[]
+  def add(self,name,price):
+    self.name.append(name)
+    self.price.append(price)
+  def show(self):
+    print(self.name[0],self.price[0])
+    print(self.name[1],self.price[1])
+    
+  
 
 
 m = Menu()  # Menu is a class
 m.add("idly", 10)
-m.add("vada". 20)
+m.add("vada", 20)
 
 m.show()
+
