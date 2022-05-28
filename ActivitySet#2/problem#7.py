588e46c8ccb42d1d5e55f305cb2fe29976fac6ad
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-05-28 17:21:51 +0000

    ..

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,10 +1,18 @@
-
-
 class Menu:
-    """fill in class definition here"""
-
+  my_menu=[]
+  def __init__(self,name='',price=0):
+    self.my_menu.append((name,price))
+  def __add__(self,a):
+      return Menu(a[0],a[1])
+  def __str__(self):
+    st=''
+    sd=self.my_menu[1:]
+    for a,b in sd:
+      st+=a+' '+str(b)+'\n'
+    return st[:-1]
 
 m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
+m = m + ("idly", 10) + ("vada", 20) + ('dosa',50)
 
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
 print(m)  # should print the menu properly
