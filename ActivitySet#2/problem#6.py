fba286ea85b3337f5674ef434d1f0fdb502057fd
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-05-28 07:42:02 +0000

    ..

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,21 +1,18 @@
-
-
 class Menu:
-  price=[]
-  name=[]
+  my_menu=[]
   def add(self,name,price):
-    self.name.append(name)
-    self.price.append(price)
+    self.my_menu.append((name,price))
   def show(self):
-    print(self.name[0],self.price[0])
-    print(self.name[1],self.price[1])
+    for a,b in self.my_menu:
+      print(a,b)
     
   
-
-
 m = Menu()  # Menu is a class
 m.add("idly", 10)
 m.add("vada", 20)
-
+m.add("dosa", 30)
+m.add("rice", 25)
 m.show()
+
+
 
