ea957bd8baa38eabf3623142a62be0119d46fc69
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-05-29 12:30:55 +0000

    ..

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -3,7 +3,7 @@ class Menu:
   def __init__(self,name='',price=0):
     self.my_menu.append((name,price))
   def __add__(self,a):
-      return Menu(a[0],a[1])
+    return Menu(a[0],a[1])
   def __str__(self):
     st=''
     sd=self.my_menu[1:]
--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,11 +1,19 @@
+class Menu(dict):
 
-
-class Menu:
-    """fill in class definition here"""
-
-
+  def __setitem__(self,key='',value=0):
+    self.__dict__[key]=value
+  def __str__(self):
+    st=''
+    for a,b in self.__dict__.items():
+      st+=a+' '+str(b)+'\n'
+    return st[:-1]
+  #represent a class's objects as a string
+  """
+  def __repr__(self):
+      return repr(self.__dict__)
+  """
 m = Menu()
 m["idly"] = 10
 m["vada"] = 20
-
+m["dosa"] = 50
 print(m)
