45a94ff04297ea79d0c16742e3b584c036f56532
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-06-05 17:38:44 +0000

    ..

--- ActivitySet#2/problem#9.py
+++ ActivitySet#2/problem#9.py
@@ -1,11 +1,18 @@
-
-
 class Menu(dict):
     """fill in class definition here"""
-
-
-class Order:
+  def __dict__(self,key='',value=0):
+    self.__dict__[key]=value
+  def __str__(self):
+    return ('\n'.join([str(a+' '+str(b)) for a,b in self.__dict__.items()]))
+    
+class Order(dict,Menu):
     """fill in class definition here"""
+  def __dict__(self,key1='',value1=0):
+    self.__dict__[key1]=value1
+    
+  
+  
+  
 
 
 class Bill:
