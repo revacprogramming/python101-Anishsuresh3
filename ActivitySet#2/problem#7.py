e54529ca19a5c4c3346ca0284335b18e76b63df6
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-06-23 03:57:58 +0000

    ..

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -5,7 +5,6 @@ class Menu:
   def __add__(self,a):
     self.my_menu.append((a[0],a[1]))
     return self
-
   def __str__(self):
     sd=self.my_menu[1:]
     return ("\n".join([str(a+' '+str(b)) for a,b in sd]))
@@ -17,6 +16,8 @@ m = m + ("idly", 10) + ("vada", 20) + ('dosa',50)
 
 # Hint: operator overloading special methods (__add__, __sub__, etc.)
 print(m)  # should print the menu properly
+m1=Menu()
+print(m1)
 
 
 
---
+++ ActivitySet#3/problem#2.py
@@ -0,0 +1,11 @@
+str1='CSE'
+str2='ECE'
+if str1 > str2:
+    print('str1 is greater than str2')
+elif str1 < str2:
+    print('str2 is greater than str1')
+else:
+    print('str1 is equal to str2')
+print(ord('CSE'))
+print(ord('ECE'))
+
