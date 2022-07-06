f59b6ffce71bb183b260a2f6ee596e3dc35e2e1d
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-07-06 10:47:25 +0000

    ..

--- ActivitySet#2/problem#9.py
+++ ActivitySet#2/problem#9.py
@@ -1,21 +1,32 @@
 class Menu(dict):
   """fill in class definition here"""
+  def __init__(self):
+    self.d={}
   def __setitem__(self,key='',value=0):
-    if(key in self.__dict__):
-        self.__dict__[key]+=value
+    if(key in self.d):
+        self.d[key]+=value
     else:
-        self.__dict__[key]=value
+        self.d[key]=value
   def __str__(self):
-    return ('\n'.join([str(a+' '+str(b)) for a,b in self.__dict__.items()]))
+    return ('\n'.join([str(a+' '+str(b)) for a,b in self.d.items()]))
     
 class Order(Menu):
+  def __init__(self,class_m):
+    self.d1={}
+    self.class_m=class_m
   def __setitem__(self,key1='',value1=0):
-    if self.__dict__[key1] in Menu.__dict__:
-        self.__dict__[key1]=value1
+    if key1 in self.class_m:
+        print('True')
+        if self.class_m[key1]>=value1:
+          self.d1[key1]=value1
+        else:
+          self.d1[key1]=self.class_m[key1]
+          print('Not enough quantity but can provide ',self.class_m[key1])
+          
      
   
   def __str__(self):
-    return str(self.__dict__)
+    return str(self.d1)
       
       
     
@@ -31,7 +42,9 @@ print(m)
 o = Order(m)
 try:
     o["vada"] = 2
+    print(o)
     o["pongal"] = 2
+    print(o)
 
 except KeyError as e:
     print(e)
