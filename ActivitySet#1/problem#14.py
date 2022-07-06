e61dd1abe16872591ebc930df027461009468551
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-07-06 06:48:41 +0000

    ..

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,15 +1,14 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import urllib.request
+import xml.etree.ElementTree as et
+fh=urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1550210.xml')
+data=fh.read()
+tree=et.fromstring(data)
+lst=tree.findall('comments/comment') 
+sum=0
+for items in lst:
+    num=int(items.find('.//count').text)
+    sum+=num
 
-import xml.etree.Elementtree as ET
-data = '''
-<person>
-<name>ABCD</name>
-<phone type = " intl ">
-</phone>
-<email hide = "yes">
-</person> '''
-
-tree = ET.fromstring(data)
-print('Name:', tree.find('name').text)
-print('Attr:', tree.find('email').get('hide'))
+print(sum)
